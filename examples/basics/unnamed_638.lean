3eb770d7494f5b87038e53e659461ad1340e6f82
Jeremy Avigad <avigad@cmu.edu>
2020-07-05 09:54:54 -0400

    Update Sun Jul  5 09:54:54 EDT 2020

--- examples/basics/unnamed_638.lean
+++ examples/basics/unnamed_638.lean
@@ -5,7 +5,7 @@ namespace my_ring
 variables {R : Type*} [ring R]
 
 -- BEGIN
-theorem neg_add_cancel_right (a b : R) : (a + b) + -b = a :=
+theorem add_neg_cancel_right (a b : R) : (a + b) + -b = a :=
 sorry
 -- END
 
