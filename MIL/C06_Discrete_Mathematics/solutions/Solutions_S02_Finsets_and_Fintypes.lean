c1fc4608f2d01dc2c9dab08dc8860e1ad5553074
Jeremy Avigad <avigad@cmu.edu>
2024-09-08 16:54:51 -0400

    Update Sun 08 Sep 2024 04:54:51 PM EDT

--- MIL/C06_Discrete_Mathematics/solutions/Solutions_S02_Finsets_and_Fintypes.lean
+++
@@ -1,9 +0,0 @@
-import Mathlib.Data.Finset.Sum
-import Mathlib.Data.Finset.Fold
-import Mathlib.Data.Finset.Powerset
-import Mathlib.Data.Finset.Prod
-import Mathlib.Data.Nat.Prime.Basic
-import Mathlib.Algebra.BigOperators.Ring
-import Mathlib.Algebra.BigOperators.Intervals
-import Mathlib.Algebra.Order.BigOperators.Group.Finset
-
